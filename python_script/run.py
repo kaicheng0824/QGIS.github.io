578f383ffe71c7047df6d54558cae46697d29691
Kai-Wen Cheng <kac003@ucsd.edu>
2023-02-23 19:25:07 -0800

    Fix Run.py

--- python_script/run.py
+++ python_script/run.py
@@ -6,14 +6,14 @@ import user_interface
 import directory_setup as setup
 
 if __name__ == "__main__":
-    # setup.create_folder('./data')
+    setup.create_folder('./data')
 
-    # iso_query = user_interface.getISO()
-    # getNodeMetadata.main(iso_query)
+    iso_query = user_interface.getISO()
+    getNodeMetadata.main(iso_query)
 
-    # starttime = user_interface.getStartTime()
-    # endtime = user_interface.getEndTime()
-    # getLMP.main(starttime,endtime)
+    starttime = user_interface.getStartTime()
+    endtime = user_interface.getEndTime()
+    getLMP.main(starttime,endtime)
 
-    # processLMP.main()
+    processLMP.main()
     csvToGeoJSON.main('NodeMetaData.csv')
